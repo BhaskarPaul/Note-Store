fc3c826f108aeccc1d87c5780aeb7733e1d72fee
Bhaskar Paul <paulbhaskar007@gmail.com>
2021-09-08 19:02:20 +0530

    bug fixes ...

--- client/src/container/actions/index.js
+++ client/src/container/actions/index.js
@@ -1,36 +1,35 @@
 import axios from "axios";
 
+const base_url = "https://note-store.herokuapp.com/";
+
 export const showTodoAsync = () => async (dispatch, getState) => {
-  const response = await axios.get("https://note-store.herokuapp.com/todos/");
+  const response = await axios.get(`${base_url}todos/`);
   const data = await response.data;
   if (data !== []) dispatch(showTodo(data));
 };
 
 export const showTodoAsyncFav = () => async (dispatch, getState) => {
-  const response = await axios.get("http://localhost:3000/todos/fav");
+  const response = await axios.get(`${base_url}todos/fav`);
   const data = await response.data;
   if (data !== []) dispatch(showTodo(data));
 };
 
 export const addTodoAsync = (data) => async (dispatch, getState) => {
-  const response = await axios.post("http://localhost:3000/todos", data);
+  const response = await axios.post(`${base_url}todos/`, data);
   const result = await response.data;
   console.log(result);
   dispatch(addTodo(data));
 };
 
 export const deleteTodoAsync = (id) => async (dispatch, getState) => {
-  const response = await axios.delete(`http://localhost:3000/todos/${id}`);
+  const response = await axios.delete(`${base_url}todos/${id}`);
   const result = await response.data;
   console.log(result);
   dispatch(deleteTodo(id));
 };
 
 export const editTodoAsync = (payload) => async (dispatch, getState) => {
-  const response = await axios.put(
-    `http://localhost:3000/todos/${payload._id}`,
-    payload
-  );
+  const response = await axios.put(`${base_url}todos/${payload._id}`, payload);
   const result = await response.data;
   console.log(result);
   dispatch(editTodo(payload));
